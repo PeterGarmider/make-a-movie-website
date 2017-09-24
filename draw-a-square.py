e271d00ad340272424ce542599104e62212b7348
Peter Garmider <petergarmider@gmail.com>
2017-09-24 01:53:01 -0400

    Completed the "draw a square" lesson. Added turtle mini project image.

--- draw-a-square.py
+++ draw-a-square.py
@@ -1,20 +1,38 @@
+
 import turtle
+import math
 
 window = turtle.Screen()
-window.bgcolor("red")
+window.bgcolor("yellow")
+
+"""def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("red")
+    #draw a square
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("blue")
+    brad.speed(10)
+"""
 def draw_square():
 
-    vehicle = turtle.Turtle()
-    vehicle.shape("turtle")
-    vehicle.color("blue")
-    vehicle.speed(10)
+    bob = turtle.Turtle()
+    bob.shape("turtle")
+    bob.color("blue")
+    bob.speed(10)
 
-    counter = 0
-    while counter < 100:
-        vehicle.forward(100)
-        vehicle.right(90)
-        counter = counter+1
+    rotation_counter = 0
+    while rotation_counter < 36:
+        bob.right(10)
+        rotation_counter = rotation_counter + 1
 
+        square_counter = 0
+        while square_counter < 4:
+            bob.forward(100)
+            bob.right(90)
+            square_counter = square_counter+1
+
+"""
 def draw_circle():
     boat = turtle.Turtle()
     boat.shape("circle")
@@ -22,31 +40,30 @@ def draw_circle():
     boat.speed(100)
 
     counter = 0
-    while counter < 100:
-        boat.circle(100)
-        boat.forward(10)
-        boat.right(90)
-        boat.forward(30)
+    while counter < 10:
         boat.circle(100)
         counter = counter+1
+"""
+
 
 def draw_triangle():
-    plane = turtle.Turtle()
-    plane.shape("triangle")
-    plane.color("purple")
-    plane.speed(10)
+    mike = turtle.Turtle()
+    mike.color("purple")
+    mike.speed(100)
 
-    counter = 0
-    while counter < 100:
-        plane.forward(200)
-        plane.right(120)
-        plane.forward(200)
-        plane.right(60)
-        plane.forward(200)
-        plane.right(120)
-        counter = counter+1
+    rotation_counter = 0
+    while rotation_counter < 300:
+        mike.right(45)
+        mike.forward(20 + rotation_counter)
+        rotation_counter = rotation_counter + 1
+
+        triangle_counter = 0
+        while triangle_counter < 4:
+            mike.right(55)
+            mike.forward(math.exp(triangle_counter+1))
+            triangle_counter = triangle_counter + 1
 
     window.exitonclick()
-draw_square()
-draw_circle()
+
 draw_triangle()
+#draw_square()
